249a2aeb74be965ba27903f098822c508650646e
clayandgithub <clayanddev@sina.com>
2016-06-28 13:47:24 +0800

    race configure

--- src/main/java/com/alibaba/middleware/race/RaceConfig.java
+++ src/main/java/com/alibaba/middleware/race/RaceConfig.java
@@ -4,24 +4,25 @@ import java.io.Serializable;
 
 public class RaceConfig implements Serializable {
 	private static final long serialVersionUID = 4441842861070190453L;
-	public static String prex_tmall = "platformTmall_";
-    public static String prex_taobao = "platformTaobao_";
-    public static String prex_ratio = "ratio_";
+	public static String teamcode = "361233bpvz";
+	public static String prex_tmall = "platformTmall_" + teamcode + "_";
+    public static String prex_taobao = "platformTaobao_" + teamcode + "_";
+    public static String prex_ratio = "ratio_" + teamcode + "_";
 
 
 //    public static String MQNameServerAddr = "192.168.1.101:9876"; 
     
-    public static String JstormTopologyName = "race";
+    public static String JstormTopologyName = "361233bpvz";
     
-    public static String MetaConsumerGroup = "group9";
+    public static String MetaConsumerGroup = "361233bpvz";
     public static String MqPayTopic = "MiddlewareRaceTestData_Pay";
     public static String MqTmallTradeTopic = "MiddlewareRaceTestData_TMOrder";
     public static String MqTaobaoTradeTopic = "MiddlewareRaceTestData_TBOrder";
     
-    public static String TairConfigServer = "10.214.192.66:5198";//192.168.1.105:5198
-    public static String TairSalveConfigServer = "";
-    public static String TairGroup = "group_1";
-    public static Integer TairNamespace = 0;
+    public static String TairConfigServer = "10.101.72.127:5198";//192.168.1.105:5198
+    public static String TairSalveConfigServer = "10.101.72.128:5198";
+    public static String TairGroup = "group_tianchi";
+    public static Integer TairNamespace = 56862;
     
     public static short PC = 0;
     public static short Wireless = 1;
