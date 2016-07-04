7c63f50956fe01c48b34329bcbf3cd237bd9f7a1
clayandgithub <clayanddev@sina.com>
2016-07-04 21:29:47 +0800

    add extra thread in RatioWriter

--- src/main/java/com/alibaba/middleware/race/bolt/RatioWriter.java
+++ src/main/java/com/alibaba/middleware/race/bolt/RatioWriter.java
@@ -1,6 +1,8 @@
 package com.alibaba.middleware.race.bolt;
 
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.TreeMap;
 
 import org.slf4j.Logger;
@@ -20,82 +22,96 @@ import backtype.storm.topology.IBasicBolt;
 import backtype.storm.topology.OutputFieldsDeclarer;
 import backtype.storm.tuple.Tuple;
 
-public class RatioWriter implements IBasicBolt{
-	private static final long serialVersionUID = -8998720475277834236L;
+public class RatioWriter implements IBasicBolt {
+    private static final long serialVersionUID = -8998720475277834236L;
 
-	private static Logger LOG = LoggerFactory.getLogger(RatioWriter.class);
-	private transient TairOperatorImpl tairOperator;
-	
-	private DecoratorTreeMap PCSumCounter;
-	private DecoratorTreeMap WirelessSumCounter;
+    private static final long WRITE_TAIR_INTERVAL = 3000;
 
-	@Override
-	public void declareOutputFields(OutputFieldsDeclarer arg0) {
-		// TODO Auto-generated method stub
-		
-	}
+    private static Logger LOG = LoggerFactory.getLogger(RatioWriter.class);
+    private transient TairOperatorImpl tairOperator;
 
-	@Override
-	public Map<String, Object> getComponentConfiguration() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    private DecoratorTreeMap PCSumCounter;
+    private DecoratorTreeMap WirelessSumCounter;
 
-	@Override
-	public void cleanup() {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public void declareOutputFields(OutputFieldsDeclarer arg0) {
+        // TODO Auto-generated method stub
 
-	@Override
-	public void execute(Tuple tuple, BasicOutputCollector collector) {
-		// TODO Auto-generated method stub
-		Long key = tuple.getLong(0);
-		Double value = tuple.getDouble(1);
-		if (value < Constants.DOUBLE_DIFF_THREHOLD) {
-		    return;
-		}
-		
-		if(tuple.getSourceStreamId().equals(RaceTopology.TMPCCOUNTERSTREAM)
-		        || tuple.getSourceStreamId().equals(RaceTopology.TBPCCOUNTERSTREAM)){
-			PCSumCounter.put(key, PCSumCounter.get(key) + value);			
-		}else{
-			WirelessSumCounter.put(key, WirelessSumCounter.get(key) + value);
-		}
-		
-		Double pcSum = 0.0;
-		Double wirelessSum = 0.0;
-		
-		for(Map.Entry<Long, Double> entry : PCSumCounter.entrySet()){
-			Long entryKey = entry.getKey();
-			
-			pcSum += PCSumCounter.get(entryKey);
-			wirelessSum += WirelessSumCounter.get(entryKey);
-			
-			if(pcSum > 1e-6){
-				double ratio = wirelessSum / pcSum;
-				tairOperator.write(RaceConfig.prex_ratio + entryKey, DoubleUtil.roundedTo2Digit(ratio));
-				LOG.info("Ratio Writer:" + entryKey + ":" + WirelessSumCounter.get(entryKey) / PCSumCounter.get(entryKey));
-			}
-			
-		}
-	}
-		
-		
+    }
 
-	@Override
-	public void prepare(Map arg0, TopologyContext arg1) {
-		// TODO Auto-generated method stub
-		tairOperator = new TairOperatorImpl(RaceConfig.TairConfigServer, RaceConfig.TairSalveConfigServer,
-                RaceConfig.TairGroup, RaceConfig.TairNamespace);
-		
-		PCSumCounter = CounterFactory.createTreeCounter();
-		WirelessSumCounter = CounterFactory.createTreeCounter();
-		
-//		for(Map.Entry<Long, Double> entry : PCSumCounter.entrySet()){
-//			Long key = entry.getKey();
-//			tairOperator.write(RaceConfig.prex_ratio + key, 0.0);
-//		}
-	}
+    @Override
+    public Map<String, Object> getComponentConfiguration() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void cleanup() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void execute(Tuple tuple, BasicOutputCollector collector) {
+        // TODO Auto-generated method stub
+        Long key = tuple.getLong(0);
+        Double value = tuple.getDouble(1);
+        if (value < Constants.DOUBLE_DIFF_THREHOLD) {
+            return;
+        }
+
+        if (tuple.getSourceStreamId().equals(RaceTopology.TMPCCOUNTERSTREAM)
+                || tuple.getSourceStreamId().equals(
+                        RaceTopology.TBPCCOUNTERSTREAM)) {
+            PCSumCounter.put(key, PCSumCounter.get(key) + value);
+        } else {
+            WirelessSumCounter.put(key, WirelessSumCounter.get(key) + value);
+        }
+    }
+
+    private void writeTair() {
+        Double pcSum = 0.0;
+        Double wirelessSum = 0.0;
+
+        Iterator<Entry<Long, Double>> iter = PCSumCounter.entrySet().iterator();
+        while (iter.hasNext()) {
+            Entry<Long, Double> entry = iter.next();
+            Long entryKey = entry.getKey();
+
+            pcSum += PCSumCounter.get(entryKey);
+            wirelessSum += WirelessSumCounter.get(entryKey);
+
+            if (pcSum > 1e-6) {
+                double ratio = wirelessSum / pcSum;
+                tairOperator.write(RaceConfig.prex_ratio + entryKey,
+                        DoubleUtil.roundedTo2Digit(ratio));
+            }
+        }
+    }
+
+    @Override
+    public void prepare(Map arg0, TopologyContext arg1) {
+        // TODO Auto-generated method stub
+        tairOperator = new TairOperatorImpl(RaceConfig.TairConfigServer,
+                RaceConfig.TairSalveConfigServer, RaceConfig.TairGroup,
+                RaceConfig.TairNamespace);
+
+        PCSumCounter = CounterFactory.createTreeCounter();
+        WirelessSumCounter = CounterFactory.createTreeCounter();
+
+        new Thread() {
+            @Override
+            public void run() {
+                while (true) {
+                    try {
+                        Thread.sleep(WRITE_TAIR_INTERVAL);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    writeTair();
+                }
+            }
+        }.start();
+    }
 
 }
